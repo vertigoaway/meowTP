9bb7a02ef0cb49958ca378c219bb52eae283184e
vertigoaway <connormm2@protonmail.com>
2025-12-08 10:52:50 -0500

    dead nonce code ill revisit later

--- crypto.py
+++ crypto.py
@@ -19,8 +19,9 @@ def createKeyPair(): #you are never gonna believe what this does
     format=serialization.PublicFormat.SubjectPublicKeyInfo)
     return privKey, pubKey, pem
 
-def pubKeyEncrypt(msg, key): #single threaded encrypt
-    msg = key.encrypt(msg,
+def pubKeyEncrypt(msg, key, nonce): #single threaded encrypt
+    print(msg)
+    msg = key.encrypt(msg+nonce.tobytes(6),
         padding.OAEP(
             mgf=padding.MGF1(algorithm=hashes.SHA256()),
             algorithm=hashes.SHA256(),
@@ -41,11 +42,12 @@ def privKeyDecrypt(msg, key):#single threaded decrypt
     return msg
 
 
-def bulkEncrypt(msgs, key): #MT encryption
+def bulkEncrypt(msgs, key, nonce): #MT encryption
     threads = []
     for i, msg in enumerate(msgs):
-        thread = threading.Thread(pubKeyEncrypt, args=[msg,key])
+        thread = threading.Thread(pubKeyEncrypt, args=[msg,key,nonce])
         threads.append(thread)
+        nonce+=1
         thread.start()
     for i,t in enumerate(threads):
         msgs[i] = t.join()
