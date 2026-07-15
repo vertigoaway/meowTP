3611f8f3e4eca22335f0cd28cd304684b18cd5d9
vertigoaway <connormm2@protonmail.com>
2026-07-15 13:59:23 -0400

    Add 1 byte flag. Compress packed message if above 128b

--- netlib.py
+++ netlib.py
@@ -1,10 +1,11 @@
 import compression.zstd as zstd
 import base64
+from multiprocessing import Value
 from typing import Any
 from typing import cast
 import msgpack as msgP
 ENCODING = 'utf8'
-
+MAX_FRAME = 64 * 1024 * 1024
 def serializeBytes(data: bytes) -> str:
     return base64.b64encode(data).decode(ENCODING)
 
@@ -12,8 +13,8 @@ def serializeBytes(data: bytes) -> str:
 def deserializeBytes(data: str) -> bytes:
     return base64.b64decode(data)
 
-
-def compress(data: dict,level : int = 3) -> bytes:
+# compress and decompress need a better name lol
+def compress(data: dict, level : int = 9) -> tuple[bytes,int]:
     """Messagepack and Zstd compress dict
     Parameters:
         data (dict): data to be packed and compressed.
@@ -22,11 +23,17 @@ def compress(data: dict,level : int = 3) -> bytes:
     Returns (bytes):
      Compressed and packed bytes.
     """
-    encodedData = cast(bytes,msgP.dumps(data))
-    return zstd.compress(encodedData, level)
+    flags = 0b00000000    
+    packed = cast(bytes,msgP.dumps(data))
+    if len(packed) >= 128:
+        print('c')
+        flags |= (1 << 0)
+        packed = zstd.compress(packed, level)
+    
+    return (packed,flags)
 
 
-def decompress(data: bytes) -> dict:
+def decompress(data: bytes,zstdEnabled: bool = True) -> dict: 
     """un-Messagepack and Zstd decompress.
     Parameters:
         data (bytes): Packed and compresssed bytes.
@@ -34,9 +41,13 @@ def decompress(data: bytes) -> dict:
     Returns (dict):
      Decompressed data.
     """
-    decodedData = zstd.decompress(data)
-    result = cast(dict,msgP.loads(decodedData))
-    return result
+    if zstdEnabled:
+        decompressedData = zstd.decompress(data)
+        print(f"\tcompressed:{len(data)}\n\tdecompressed{len(decompressedData)}")
+    else:
+        decompressedData = data
+    unpackedData = cast(dict,msgP.loads(decompressedData))
+    return unpackedData
 
 
 def recvExact(sock, n) -> bytes:
@@ -51,14 +62,22 @@ def recvExact(sock, n) -> bytes:
 
 def recvUnencryptedFrame(sock) -> dict[Any, Any]:
     length = int.from_bytes(recvExact(sock, 4), "big")
+    if length > MAX_FRAME:
+        raise ValueError("Frame too large!")
+    flags = int.from_bytes(recvExact(sock,1),'big')
     payl = recvExact(sock, length)
-    return decompress(payl)
+    if flags & (1 << 0): #compression enabled
+        res = decompress(payl,zstdEnabled=True)
+    else:
+        res = decompress(payl, zstdEnabled=False)
+    return res
 
 
 def sendUnencryptedFrame(sock, payload) -> None:
-    payload = compress(payload)
-    sock.sendmsg([len(payload).to_bytes(4, "big"),
-                  payload])
+    frame,flags = compress(payload)
+    sock.sendmsg([len(frame).to_bytes(4, "big"),
+                  flags.to_bytes(1,"big"),
+                  frame])
     return
 
 
