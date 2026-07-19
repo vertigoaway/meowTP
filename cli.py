a66b144559ae47b13f9f48b280b2113e0cda4cff
vertigoaway <connormm2@protonmail.com>
2026-07-19 16:15:27 -0400

    Implement client ping command and remove direct netlib calls from commands

--- cli.py
+++ cli.py
@@ -2,12 +2,21 @@ import socket
 from typing import Any, cast
 from netlib import recvUnencryptedFrame, sendUnencryptedFrame
 import logging
-
+import time
 logger = logging.getLogger(__name__)
 
 
 class client:
+
+    srv : tuple[str,int]
+    sock : socket.socket
+    frameId : int
+    reqId : int
     def open(self, srv: tuple[str, int] | None = None):
+        """Opens a socket connection to the server.
+        Parameters:
+         srv: Tuple containing hostname/ip and port number.
+          Defaults to last used if left empty."""
         if isinstance(srv, type(None)):
             srv = self.srv
         self.srv = srv
@@ -16,8 +25,24 @@ class client:
         return
 
     def __init__(self, srv: tuple[str, int]):
+        """Creates client object and opens a connection to the server.
+        Parameters:
+         srv: Tuple containing hostname/ip and port number.
+        """
         self.open(srv)
+        self.reqId = 1
         return
+    
+    def sendReq(self,req,responseExpected=True) -> dict[Any, Any]:
+        req["id"] = self.reqId
+        sendUnencryptedFrame(self.sock,req)
+        self.reqId += 1
+        if responseExpected:
+            resp = recvUnencryptedFrame(self.sock)
+        else:
+            return {} # for the sake of my sanity
+
+        return resp
 
     def query(self, key: str):
         """Find V corressponding to key arg in server.
@@ -28,8 +53,7 @@ class client:
          out: Value corresponding to key or status code if failed.
         """
         req = {"cmd": "query", "query": {"k": key}}
-        sendUnencryptedFrame(self.sock, req)
-        received = recvUnencryptedFrame(self.sock)
+        received = self.sendReq(req)
         if received["status"] == 200:
             return received["result"]["v"]
         else:
@@ -44,8 +68,7 @@ class client:
          out: Keys corresponding to val or status code if failed.
         """
         req = {"cmd": "query", "query": {"v": val}}
-        sendUnencryptedFrame(self.sock, req)
-        received = recvUnencryptedFrame(self.sock)
+        received = self.sendReq(req)
 
         if received["status"] == 200:
             return received["result"]["k"]
@@ -69,18 +92,30 @@ class client:
         else:
             cmd = "post"
         req = {"cmd": cmd, cmd: {"k": key, "v": value}}
-        sendUnencryptedFrame(self.sock, req)
-
-        res = recvUnencryptedFrame(self.sock)["status"]
+        res = self.sendReq(req)["status"]
         if res == 201:
             return True
-        elif res is 400:
+        elif res == 400:
             logger.error(f"invalid post parameters!: {req}")
             raise TypeError
-        elif res is 304:
+        elif res == 304:
             return False
         return False
+    def ping(self) -> float:
+        """Pings server.
 
+        Returns (float): 
+         Difference between function call and first packet arrival on server side."""
+        begin = time.time()
+        req = {'cmd':'ping','ping': {'time':begin}}
+        res = self.sendReq(req)['result']
+        end = res['time']
+        delta = res['delta']
+        change = end - begin
+        if change != delta:
+            logger.critical(f'C->S ping delta mismatch! calculated: {change} received {delta}')
+        logger.info(f'C->S ping: {change*1000}ms')
+        return change
     def close(self) -> None:
         """Closes client socket."""
         logger.info(f"socket close fired")
