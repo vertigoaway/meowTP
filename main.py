377c214fb7453228cb6a9ea489eb275187a78c18
vertigoaway <connormm2@protonmail.com>
2026-07-19 16:18:14 -0400

    Test ping and post

--- main.py
+++ main.py
@@ -26,15 +26,11 @@ if __name__ == "__main__":
 
         server_thread.daemon = True
         server_thread.start()
-        logging.info("Server loop running in thread:", server_thread.name)
+        #logging.info("Server loop running in thread:", str(server_thread.name))
 
         while True:
             c1 = cli.client((ip, port))  # pyright: ignore[reportArgumentType]
-            aa = base64.b64encode(random.randbytes(8)).decode(ENCODING)
-            meow = base64.b64encode(random.randbytes(8)).decode(ENCODING)
-            x = c1.post(aa, meow)
-            c1.close()
-            c1.open()
-            y = c1.query("apple")
+            x = c1.post('abcdefghijklmnopqrstuvwxyzlhf;isfsdferfneiufhrufwnrwefjwpfjdklvdkgjaspfjasdklfaljksrfkljas','asfiofvjklwremvpasjbviafvmwiefpasdojfiasdjfaskl;dfjoiwejqlk;fnsdlmvndfoibuhreiouydsflkgjnlkjbhsifrutsi')
+            print(f"{c1.ping()*1000}ms")
             c1.close()
             c1 = None
